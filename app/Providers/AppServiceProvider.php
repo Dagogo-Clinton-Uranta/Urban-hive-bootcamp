e48ae8f2c685d9552ef7720a2d052ad946d466e1
Dagogo Clinton Uranta <dagogouranta@gmail.com>
2022-04-21 21:05:20 +0100

    reverting to https ONLY

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
        //delete the condition below if youre having problems
-      // \Illuminate\Support\Facades\URL::forceScheme('https');
+       \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
